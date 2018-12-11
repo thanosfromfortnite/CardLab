58abc9a8f2c5d5d07972ed1050d052e69f515a6b
BT_1N3_26 <g>
2018-12-11 11:23:25 -0500

    i

--- src/Deck.java
+++ src/Deck.java
@@ -30,4 +30,17 @@ public class Deck {
         }
         else return null;
     }
+
+    public void shuffle() {
+        for (int i = 0; i < dealt.size(); i ++) {
+            unDealt.add(dealt.get(0));
+            unDealt.remove(i);
+        }
+        for (int i = 0; i < unDealt.size(); i ++) {
+            int r = (int) (Math.random() * unDealt.size());
+            Card c = unDealt.get(r);
+            unDealt.remove(i);
+            unDealt.add(c);
+        }
+    }
 }
