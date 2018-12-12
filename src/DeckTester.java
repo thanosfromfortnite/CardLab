d8a1e820945659eef05f2555d243b6061cb497e6
BT_1N3_07 <as>
2018-12-12 09:57:54 -0500

    drfgdfgdfgdfg

--- src/DeckTester.java
+++ src/DeckTester.java
@@ -6,18 +6,23 @@ public class DeckTester {
         int[] powerValues = new int[53];
         String[] listOfRanks = {"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
         String[] listOfSuits = {"Diamonds", "Clubs", "Hearts", "Spades"};
-        for (int i = 0; i < listOfRanks.length; i++) {
-            for (int j = 0; j < listOfSuits.length; j++) {
+        for (int i = 0; i < listOfRanks.length; i ++) {
+            for (int j = 0; j < listOfSuits.length; j ++) {
                 ranks[(i * 4) + j] = listOfRanks[i];
                 suits[(i * 4) + j] = listOfSuits[j];
                 powerValues[(i * 4) + j] = (i * 4) + j + 1;
             }
         }
         Deck deck = new Deck(ranks, suits, powerValues);
+        System.out.println("UNSHUFFLED");
+        for (int i = 0; i < 52; i  ++) {
+            System.out.println(deck.deal().toString());
+        }
 
         deck.shuffle();
 
-        for (int i = 0; i < 52; i++) {
+        System.out.println("SHUFFLED");
+        for (int i = 0; i < 52; i ++) {
             System.out.println(deck.deal().toString());
         }
     }
